1acc69bb4025ebb00f4d621cf976367b32307f16
N.Zotov <nikolai.zotov1@simbirsoft.com>
2022-09-10 14:33:57 +0400

    changed gradle

--- src/test/java/tests/BrowserStackAndroidSelenideTests.java
+++ src/test/java/tests/BrowserStackAndroidSelenideTests.java
@@ -18,7 +18,7 @@ import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 
 
-@Tag("selenide_android")
+
 public class BrowserStackAndroidSelenideTests extends TestBase {
     @Epic("Поиск")
     @Feature("Поиск на главной странице")
